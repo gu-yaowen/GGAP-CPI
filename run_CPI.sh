2599d6212a6c297f295b85c8e05f42f2cae19596
Yaowen Gu <82702241+gu-yaowen@users.noreply.github.com>
2024-01-02 00:15:04 -0500

    Delete run_CPI.sh

--- run_CPI.sh
+++
@@ -1,9 +0,0 @@
-# Description: Run CPI experiments
-model=$1
-echo $model
-python main.py --gpu 0 \
-               --data_path data/MoleculeACE/CPI_plus_Integrated.csv \
-                --mode baseline_CPI \
-                --dataset_type regression \
-                --seed 0 \
-                --baseline_model $model
